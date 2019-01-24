8b59b52e0f191b471908008469c7170a5238b5fa
Steven Kaufman <steveplaysguiola@gmail.com>
2019-01-24 13:08:08 -0600

    Update 4.cpp

--- wk2/4.cpp
+++ wk2/4.cpp
@@ -4,15 +4,21 @@ using namespace std;
 
 int main(){
     float feet, yards, inches, cm, meters;
+    
     cout << "Enter number of feet: ";
     cin >> feet;
-    yards = roundf(feet / 3 * 100) / 100;
+    
+    yards = int(feet / 3 * 100 + .5) / 100;
     printf("= %.2f yards\n", yards);
-    inches = roundf(feet * 12 * 100) / 100;
+    
+    inches = int(feet * 12 * 100 + .5) / 100;
     printf("= %.2f inches\n", inches);
-    cm = roundf(feet * 12 * 2.54 * 100) / 100;
+    
+    cm = int(feet * 12 * 2.54 * 100 + .5) / 100;
     printf("= %.2f cm\n", cm);
-    meters = roundf(feet * 12 * 2.54 / 100 * 100) / 100;
+    
+    meters = int(feet * 12 * 2.54 + .5) / 100;
     printf("= %.2f meters\n", meters);
+    
     return 0;
 }
