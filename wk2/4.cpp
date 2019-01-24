200da2b8e0f3c57438c77fb09a02400369366b47
Steven Kaufman <steveplaysguiola@gmail.com>
2019-01-24 13:18:15 -0600

    readded roundf

--- wk2/4.cpp
+++ wk2/4.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <stdio.h>
+#include <math.h>
 
 using namespace std;
 
@@ -8,16 +10,16 @@ int main(){
     cout << "Enter number of feet: ";
     cin >> feet;
     
-    yards = int(feet / 3 * 100 + .5) / 100;
+    yards = roundf(feet / 3 * 100) / 100;
     printf("= %.2f yards\n", yards);
     
-    inches = int(feet * 12 * 100 + .5) / 100;
+    inches = roundf(feet * 12 * 100) / 100;
     printf("= %.2f inches\n", inches);
     
-    cm = int(feet * 12 * 2.54 * 100 + .5) / 100;
+    cm = roundf(feet * 12 * 2.54 * 100) / 100;
     printf("= %.2f cm\n", cm);
     
-    meters = int(feet * 12 * 2.54 + .5) / 100;
+    meters = roundf(feet * 12 * 2.54) / 100;
     printf("= %.2f meters\n", meters);
     
     return 0;
